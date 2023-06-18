25586cd4b51e52fde9b946c4f92aaf270685b0bf
deltathrs <deltatherof@gmail.com>
2023-06-18 13:40:46 +0700

    Initial commit

--- resources/views/dashboard/layout.blade.php
+++ resources/views/dashboard/layout.blade.php
@@ -17,9 +17,30 @@
   <link rel="stylesheet" href="{{ asset('admin') }}/css/style.css">
   <!-- endinject -->
   <link rel="shortcut icon" href="{{ asset('admin') }}/images/favicon.png" />
-
+  <!-- tokenfield -->
+  <link rel="stylesheet" type="text/css"
+      href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.20/summernote-bs5.min.css" />
+  <link rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tokenfield/0.12.0/css/bootstrap-tokenfield.css"
+    integrity="sha512-wcf2ifw+8xI4FktrSorGwO7lgRzGx1ld97ySj1pFADZzFdcXTIgQhHMTo7tQIADeYdRRnAjUnF00Q5WTNmL3+A=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+<style>
+    .tokenfield .token {
+        margin: -1px 1px 1px 1px;
+        height: 25px;
+        line-height: 22px;
+        color: #fff;
+        background-color: #0b5ed7
+    }
+
+    .tokenfield .token a {
+        color: #FFFFFF;
+        text-decoration: none;
+    }
+</style>
 </head>
 <body>
   <div class="container-scroller">
@@ -75,6 +96,24 @@
             <a class="nav-link" href="{{ route('education.index') }}">
               <i class="mdi mdi-file-document-box-outline menu-icon"></i>
               <span class="menu-title">Education</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('skill.index') }}">
+              <i class="mdi mdi-file-document-box-outline menu-icon"></i>
+              <span class="menu-title">Skill</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('profile.index') }}">
+              <i class="mdi mdi-file-document-box-outline menu-icon"></i>
+              <span class="menu-title">Profile</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('pengaturanhalaman.index') }}">
+              <i class="mdi mdi-file-document-box-outline menu-icon"></i>
+              <span class="menu-title">Pengaturan Halaman</span>
             </a>
           </li>
         </ul>
@@ -135,6 +174,10 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.20/summernote-bs5.min.js"></script>
 
+  <!-- tokenfield -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tokenfield/0.12.0/bootstrap-tokenfield.js"></script>
+
 
     <script>
         $(document).ready(function() {
@@ -143,6 +186,19 @@
         });
     });
   </script>
+  @if (isset($skill))
+    <script>
+      $(document).ready(function() {
+        $('.skill').tokenfield({
+            autocomplete: {
+                source: [{!!$skill}],
+                delay: 100
+            },
+            showAutocompleteOnFocus: true
+        });
+    });
+</script>
+  @endif
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,18 @@
 <?php
 
+use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\authController;
+use App\Http\Controllers\depanController;
+use App\Http\Controllers\educationController;
+use App\Http\Controllers\experienceController;
+use App\Http\Controllers\halamanController;
+use App\Http\Controllers\pengaturanHalamanController;
+use App\Http\Controllers\profileController;
+use App\Http\Controllers\skillController;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\authController;
 use Laravel\Socialite\Facades\Socialite;
-use App\Http\Controllers\halamanController;
-use App\Http\Controllers\educationController;
-use App\Http\Controllers\Admin\UserController;
-use App\Http\Controllers\experienceController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,9 +25,11 @@ use App\Http\Controllers\experienceController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/',[depanController::class,"index"]);
 
 Route::redirect('home','dashboard');
 
@@ -39,6 +45,12 @@ Route::prefix('dashboard')->middleware('auth')->group(
         Route::resource('halaman',halamanController::class);
         Route::resource('experience',experienceController::class);
         Route::resource('education',educationController::class);
+        Route::get('skill', [skillController::class, "index"])->name('skill.index');
+        Route::post('skill', [skillController::class, "update"])->name('skill.update');
+        Route::get('profile', [profileController::class, "index"])->name('profile.index');
+        Route::post('profile', [profileController::class, "update"])->name('profile.update');
+        Route::get('pengaturanhalaman', [pengaturanHalamanController::class, "index"])->name('pengaturanhalaman.index');
+        Route::post('pengaturanhalaman', [pengaturanHalamanController::class, "update"])->name('pengaturanhalaman.update');
 
     }
 );
